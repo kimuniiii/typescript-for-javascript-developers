9064aa03734c3bc562c7f42a0f9059614f40d5c4
kimuniiii <kk.baseball.0824@gmail.com>
2020-09-05 16:29:07 +0900

    null型・undefined型

---
+++ src/null-and-undefined.ts
@@ -0,0 +1,11 @@
+export {};
+
+//! ===================
+//! null型・undefined型
+//! ===================
+
+let absence : null = null;
+// absence = 1;
+
+let data :undefined = undefined;
+// data = "string";

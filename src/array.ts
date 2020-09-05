a4ed38ebb2498f1793344c8021f57f23516a2239
kimuniiii <kk.baseball.0824@gmail.com>
2020-09-05 15:16:36 +0900

    array型

---
+++ src/array.ts
@@ -0,0 +1,40 @@
+export {};
+
+//! =============
+//! number型
+//! =============
+
+// Array型（推奨されている書き方）
+let numbers: number[] = [1,2,3];
+
+// Array型（別の書き方 = 非推奨）
+// number型の要素を持つ配列
+let numbers2 : Array<number> = [1,2,3];
+
+//! ==============
+//! String型
+//! ==============
+
+// Array型（推奨されている書き方）
+let string : string[] = ["A","B","C"];
+
+// Array型
+// string型の要素を持つ配列
+let strings : Array<string> = ["Tokyo","Osaka","Kyoto"];
+
+//! ============
+//! 2次元配列
+//! ============
+
+// Array型
+// number型の要素を持つ2次元配列
+let arr : number[][] = [
+  [1,2],
+  [3,4,]
+]
+
+//! ======================
+//! 色々な型を持つ要素の配列
+//! ======================
+
+let hybrid : (string | number | boolean)[] = [1,false,'string'];

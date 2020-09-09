5c4b32a66867220d671deed240008fb8e333601d
kimuniiii <kk.baseball.0824@gmail.com>
2020-09-09 19:48:50 +0900

    オーバーロード・シグネチャ

---
+++ src/overloads.ts
@@ -0,0 +1,33 @@
+export {};
+
+//! ==============
+//! オーバーロード
+//! ==============
+
+//? オーバーロードとは？
+// 同じ名前の関数を複数定義すること
+// 関数の実体の方では「型制約」を行わない
+
+//? シグネチャとは？
+// 関数の名前・引数の数や型・返り値の型などをひとまとめにした呼び方
+// シグネチャで「型制約」を行う
+
+//! シグネチャ（引数と返り値の「型」だけを定義）
+function double(value : number) : number;
+function double(value : string) : string
+
+function double(value : any) : any {
+  console.log(typeof value);
+
+  if (typeof value === 'number') {
+    return value * 2;
+  }
+
+  if (typeof value === 'string') {
+    return `${value} ${value};`
+  }
+
+}
+
+console.log(double(100));
+console.log(double('GO'));

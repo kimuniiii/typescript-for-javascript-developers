eda8ba59a7a541d1a671127f2562537e20e6d56d
kimuniiii <kk.baseball.0824@gmail.com>
2020-09-12 22:31:08 +0900

    TypeScript独自のconstructorメソッドの初期化処理の書き方

---
+++ src/more-constructor.ts
@@ -0,0 +1,28 @@
+export {};
+
+//! ====================
+//! constructorを使い倒す
+//! ====================
+
+//? TypeScript独自のコンストラクタの書き方
+// 引数にアクセス修飾子を書くことで自動で初期化処理を行ってくれる
+// この言語仕様を利用してどうコードを書けばいいのか以下に記す
+
+class Person {
+  constructor(public name : string, protected age: number) {}
+}
+
+//! 上記構文は以下の構文と内部的には同じ処理を行う
+
+// class Person {
+//   public name : string
+//   protected age : number;
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age
+//   }
+// }
+
+const me = new Person("Ham",33);
+console.log(me);

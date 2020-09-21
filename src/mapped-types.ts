f6c7d5a160d4af69a5ea5c13128f70d5cb7ae80e
kimuniiii <kk.baseball.0824@gmail.com>
2020-09-22 01:29:09 +0900

    Mapped Types

---
+++ src/mapped-types.ts
@@ -0,0 +1,25 @@
+export {};
+
+//! =============
+//! Mapped Types
+//! =============
+
+//? Mapped Typesとは？
+// { [ P in K ]: T } という構文を持つ型
+// { [ P in K ]: T } = { K: T; K: T; }
+
+//? ハムさんメモ
+// 既存のオブジェクトの型を流用して新たに型を定義することが出来る
+// 具体的には「in keyof」
+// 大事なことは「あるオブジェクトのプロパティの名前から型を作ってそれを1つずつ取り出している」ことを理解できること
+
+type Profile = {
+  name : string;
+  age : number;
+}
+
+type PartialProfile = Partial<Profile>;
+type PropertyTypes = keyof Profile;
+
+type Optional<T> = { [P in keyof T]?: T[P] | null};
+type OptionalProfile = Optional<Profile>;

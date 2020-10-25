4100a05d9b9752f042dcd1e5d25652f3e9d9c8e4
木村一貴 <k-kazuki@kimura-kazuki-MacBook-Pro.local>
2020-10-26 00:40:41 +0900

    コメントの修正

--- src/array-type/array.ts
+++ src/array-type/array.ts
@@ -1,26 +1,31 @@
 export {};
 
-//! =========
-//! number型
-//! =========
+//! =====================
+//! number型の要素を持つ配列
+//! =====================
 
 // Array型（推奨されている書き方）
 // number型の要素を持つ配列
+
 let numbers: number[] = [1,2,3];
 
 // Array型（別の書き方 = 非推奨）
 // number型の要素を持つ配列
+
 let numbers2 : Array<number> = [1,2,3];
 
-//! =========
-//! String型
-//! =========
+//! =====================
+//! String型の要素を持つ配列
+//! =====================
 
 // Array型（推奨されている書き方）
+// string型の要素を持つ配列
+
 let string : string[] = ["A","B","C"];
 
 // Array型
 // string型の要素を持つ配列
+
 let strings : Array<string> = ["Tokyo","Osaka","Kyoto"];
 
 //! ========
@@ -38,5 +43,7 @@ let arr : number[][] = [
 //! ======================
 //! 色々な型を持つ要素の配列
 //! ======================
+
+// string型 or number型 or boolean型の要素を持つ配列
 
 let hybrid : (string | number | boolean)[] = [1,false,'string'];

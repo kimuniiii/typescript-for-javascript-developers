03780f1d0ff5ca99ec6463c6d2bea5f16844547f
木村一貴 <k-kazuki@kimura-kazuki-MacBook-Pro.local>
2020-10-29 02:52:30 +0900

    ファイルの移動 コードのドキュメント化

---
+++ src/function-type/function.ts
@@ -0,0 +1,18 @@
+export {};
+
+// 関数の型を付けるのは？
+//「引数・戻り値」に型を付ける
+
+/**
+ * 肥満度を表す体格指数であるBMIを求める関数
+ *
+ * @param {number} height
+ * @param {number} weight
+ * @return {number} bmi数値
+ */
+
+function bmi(height: number, weight: number): number {
+  return weight / (height * height);
+}
+
+console.log(bmi(1.78, 86));

d1d973b212511ca94b11b09889a2b6265ac904be
木村一貴 <k-kazuki@kimura-kazuki-MacBook-Pro.local>
2020-10-29 00:16:02 +0900

    type-safetyファイルの移動

--- src/type-alias/type-safety.ts
+++ src/type-alias/type-safety.ts
@@ -1,10 +1,10 @@
 export {};
 
-//! =========
+//! ======
 //! 型安全
-//! =========
+//! ======
 
 // TypeScriptをなぜ学んだほうがいいのか？
 // TypeScriptによって「型安全な仕組み」を導入し「バグを減らす」ため
 
-let isFinished : boolean = true;
+let isFinished: boolean = true;

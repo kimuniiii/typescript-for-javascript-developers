d4ab63aa143bbfd6bef852912cd029033d2af3c1
木村一貴 <k-kazuki@kimura-kazuki-MacBook-Pro.local>
2020-10-28 16:08:32 +0900

    unknown-typeディレクトリの作成とファイルの移動
    unknown型の基本的な記法の使い方の確認

--- src/unknown-type/unknown.ts
+++ src/unknown-type/unknown.ts
@@ -4,38 +4,45 @@ export {};
 //! unknown型・any型・never型
 //! ========================
 
-// 型が不定の時
+//? 型が不定の時
 // TypeScriptでは「any」または「unknown」という型を使う
 
-// any・unknownについて
-// any型・unknown型はどのような値も代入できる
+//? any・unknownの共通点は？
+// any型・unknown型、共にどのような型の値も代入できる
 
-// any・unknownの違いは？
+//? any・unknownの違いは？
 // any型・・・any型に代入したオブジェクトのプロパティ・メソッドを使用できる（TypeScriptが型のチェックを放棄した型）
 // unknown型・・・unknown型に代入したオブジェクトのプロパティ・メソッドを使用できない・実行できない
 
-// neverについて
-// never型はどの値も代入できない
+//? neverについて
+// never型はどの型の値も代入できない
 
-const kansu = () : number => {
+const kansu = (): number => {
   return 43;
-}
+};
 
-let numberAny : any = kansu();
-let numberUnknown : unknown = kansu();
+let numberAny: any = kansu();
+let numberUnknown: unknown = kansu();
 
 let sumAny = numberAny + 10;
+
+// 数値の足し算はできない
+// let sumUnknown = numberUnknown + 10;
 
 //! ============
 //! typeof型
 //! ============
 
 // typeof型
-//「typeof 変数」と書くと「その変数の型」が得られる
+//「typeof 変数」と書くと「その変数の型を表す文字列」が得られる
 
 console.log(typeof numberUnknown); // return {number}
 
-//? typeofを使ってある特定の型であることを確認しながらコードを安全に実行させる仕組みを何と呼ぶ？
+//! =============
+//! タイプガード
+//! =============
+
+//? typeofなどを使ってある特定の型であることを確認しながらコードを安全に実行させる仕組みを何と呼ぶ？
 //? タイプガード（型ガード）
 //? こうすることで「ランタイムエラー」を極力回避するコードを実装することが出来る
 

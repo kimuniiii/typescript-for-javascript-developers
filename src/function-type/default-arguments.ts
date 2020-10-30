a0209d2b490da37d4d6882962bbd02e9f0e1bf0e
木村一貴 <k-kazuki@kimura-kazuki-MacBook-Pro.local>
2020-10-30 18:40:34 +0900

    デフォルト引数の復習

--- src/function-type/default-arguments.ts
+++ src/function-type/default-arguments.ts
@@ -4,10 +4,10 @@ export {};
 //! デフォルト引数
 //! =============
 
-// デフォルト引数を宣言するためには？
+//? デフォルト引数を宣言するためには？
 //「仮引数 = デフォルト値」の形式で「仮引数」を宣言するだけ
 
-// デフォルト値が適用されるのは？
+//? デフォルト値が適用されるのは？
 //「引数が明示的に渡されなかったとき・undefinedを明示的に渡した時」だけ
 
 /**
@@ -18,8 +18,11 @@ export {};
  * @returns {number} 算術結果
  */
 
-const nextYearSalary = (currentSalary: number = 1000, taxRate: number = 1.1) => {
+const nextYearSalary = (
+  currentSalary: number = 1000,
+  taxRate: number = 1.1
+) => {
   return currentSalary * taxRate;
-}
+};
 
 console.log(nextYearSalary(5, undefined));

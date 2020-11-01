a62e1d764490fd0c143008607559c81072457282
木村一貴 <k-kazuki@kimura-kazuki-MacBook-Pro.local>
2020-11-01 16:34:21 +0900

    TypeScript独自のコンストラクタの書き方を使ってconstructorを実装

--- src/class-type/more-constructor.ts
+++ src/class-type/more-constructor.ts
@@ -9,20 +9,20 @@ export {};
 // この言語仕様を利用してどうコードを書けばいいのか以下に記す
 
 class Person {
-  constructor(public name : string, protected age: number) {}
+  constructor(public name: string, protected age: number) {}
 }
 
 //! 上記構文は以下の構文と内部的には同じ処理を行う
 
 // class Person {
-//   public name : string
-//   protected age : number;
+//   public name: string;
+//   protected age: number;
 
 //   constructor(name: string, age: number) {
 //     this.name = name;
-//     this.age = age
+//     this.age = age;
 //   }
 // }
 
-const me = new Person("Ham",33);
+const me = new Person('Ham', 33);
 console.log(me);

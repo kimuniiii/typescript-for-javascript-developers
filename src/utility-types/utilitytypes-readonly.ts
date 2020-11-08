8a7b08ce81261c236c22dbce28351453f2dc0ed9
木村一貴 <k-kazuki@kimura-kazuki-MacBook-Pro.local>
2020-11-08 19:42:20 +0900

    Readonly<T>の型の作り方に関する復習

--- src/utility-types/utilitytypes-readonly.ts
+++ src/utility-types/utilitytypes-readonly.ts
@@ -1,20 +1,22 @@
 export {};
 
+//! ============
+//! Readonly<T>
+//! ============
+
 type Profile = {
   name: string;
   age: number;
-}
+};
 
 const me: Profile = {
   name: 'Ham',
   age: 43,
-}
+};
 
-type PersonalDataTypes = Readonly<Profile>
+type Readonly<T> = { readonly [P in keyof T]: T[P] };
+type PersonalDataTypes = Readonly<Profile>;
 
 me.age++;
 
 console.log(me);
-
-type YomitoriSenyo<T> = { readonly [P in keyof T] : T[P]; }
-type YomitoriSenyoProfile = YomitoriSenyo<Profile>
